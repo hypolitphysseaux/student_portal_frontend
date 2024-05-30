7e509ffbd32d529d092f6e05926d654a9565b3fd
hypolitphysseaux <xpetkes@stuba.sk>
2024-05-30 22:30:11 +0200

    Dizajnovanie Note

--- fei-apps/src/lib/Notes.svelte
+++ fei-apps/src/lib/Notes.svelte
@@ -1,23 +1,23 @@
 <script lang="ts">
     import {onMount} from "svelte";
-    import Swiper from 'swiper/bundle';
-    import 'swiper/css/bundle';
     import '@material/web/button/filled-tonal-button.js'
     import '@material/web/button/filled-button.js'
 
-    onMount(() => {
-        const swiper = new Swiper('.swiper', {
-            slidesPerView: 1,
+    import '@material/web/iconbutton/icon-button.js'
+    import '@material/web/fab/fab.js'
 
-            pagination: {
-                el: '.swiper-pagination',
-            },
-            navigation: {
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev'
-            },
-        });
+
+    onMount(() => {
+        //Nacitat moje poznamky z databazy
+        let notes = [];
+
+        //Zobrazit dolezite ako prve
     });
+
+    async function addNote(){}
+    async function deleteNote(){}
+    async function deleteAllNotes(){}
+    async function toggleStar(){}
 
     export let isDarkModeEnabled;
 </script>
@@ -31,29 +31,60 @@
     </div>
 
     <div class="notes">
+        <!-- {each} ? -->
 
-        <div class="swiper">
-            <!-- Additional required wrapper -->
-            <div class="swiper-wrapper">
-                <!-- Slides -->
-                <div class="swiper-slide">Slide 1</div>
-                <div class="swiper-slide">Slide 2</div>
-                <div class="swiper-slide">Slide 3</div>
+        <div class="note">
+            <div class="star-mark">
+                <md-icon-button on:click={toggleStar}>
+                    <i class='bx bx-star'></i>
+                </md-icon-button>
             </div>
 
-            <!-- If we need navigation buttons -->
-            <div class="swiper-button-prev"></div>
-            <div class="swiper-button-next"></div>
-        </div>
-        
-        <md-filled-tonal-button>
-            Pridať poznámku
-        </md-filled-tonal-button>
+            <div class="delete-button">
+                <md-icon-button on:click={deleteNote}>
+                    <i class='bx bxs-trash-alt'></i>
+                </md-icon-button>
+            </div>
 
-        <md-filled-button>
-            Odstrániť všetky poznámky
-        </md-filled-button>
+            <div class="note-title">
+                Title
+            </div>
+
+        </div>
+
+
+        <div class="note">
+            <div class="star-mark">
+                <md-icon-button on:click={toggleStar}>
+                    <i class='bx bx-star'></i>
+                </md-icon-button>
+            </div>
+
+            <div class="delete-button">
+                <md-icon-button on:click={deleteNote}>
+                    <i class='bx bxs-trash-alt'></i>
+                </md-icon-button>
+            </div>
+
+            <div class="note-title">
+                Title
+            </div>
+
+        </div>
+
     </div>
+
+    <md-filled-tonal-button
+        on:click={addNote}
+    >
+        Pridať poznámku
+    </md-filled-tonal-button>
+
+    <md-filled-button
+        on:click={deleteAllNotes}
+    >
+        Odstrániť všetky poznámky
+    </md-filled-button>
 </div>
 
 
@@ -76,12 +107,56 @@
     }
 
     .notes-container .notes {
-        /*
-        display: grid;
-        grid-template-columns: repeat(auto-fit, minmax(290px, 1fr));
-        gap: 15px;
-         */
         margin-top: 150px;
+        margin-bottom: 20px;
+
+        display: flex;
+        flex-direction: row;
+        gap: 15px;
     }
 
+    .notes-container .notes .note{
+        height: 250px;
+        width: 400px;
+
+        background: var(--search-background);
+        border: 2px solid var(--navbar-icon-color);
+        border-radius: 8px;
+
+        position: relative;
+
+    }
+
+    .notes-container .notes .note .delete-button{
+        position: absolute;
+        top: 5px;
+        right: 5px;
+        --md-icon-button-hover-state-layer-color: red;
+    }
+
+    .notes-container .notes .note .delete-button i{
+        color: #f35353;
+    }
+
+    .notes-container .notes .note .star-mark{
+        position: absolute;
+        top: 5px;
+        left: 5px;
+        --md-icon-button-hover-state-layer-color: goldenrod;
+    }
+
+    .notes-container .notes .note .star-mark i{
+        color: goldenrod;
+    }
+
+    .notes-container .notes .note .note-title{
+        position: absolute;
+        top: 5px;
+        left: 50%;
+        transform: translateX(-50%);
+        overflow: hidden;
+
+        font-size: 30px;
+        color: #1d1d1d;
+    }
 </style>
