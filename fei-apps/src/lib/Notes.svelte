0172012cca3d151b82428f4364590060f4dfc8b7
hypolitphysseaux <xpetkes@stuba.sk>
2024-06-11 19:01:42 +0200

    Funkcionalita poznámok

--- fei-apps/src/lib/Notes.svelte
+++ fei-apps/src/lib/Notes.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
     import {onMount} from "svelte";
     import { fade } from 'svelte/transition';
-    import {doc, getFirestore, onSnapshot, updateDoc} from "firebase/firestore";
+
+    import { v4 as uuidv4 } from 'uuid';
+    import {doc, getFirestore, onSnapshot, updateDoc, getDoc, arrayUnion, setDoc, arrayRemove} from "firebase/firestore";
     import firebaseApp from "../firebase";
+
     import '@material/web/button/filled-tonal-button.js'
     import '@material/web/button/filled-button.js'
-
     import '@material/web/iconbutton/icon-button.js'
     import '@material/web/fab/fab.js'
 
@@ -34,23 +36,126 @@
 
     });
 
-    async function addNote(){}
+    async function addNote(){
+        try {
+            const userNotesRef = doc(db, "notes", loggedUser.uid);
+            const userNotesDoc = await getDoc(userNotesRef);
+
+            if (!userNotesDoc.exists()) {
+                await setDoc(userNotesRef, { // Use setDoc to create the document
+                    notes: [{
+                        noteId: uuidv4(),
+                        title: "Nadpis",
+                        isImporatant: false,
+                        content: "Sem zadajte text.",
+                        created: new Date()
+                    }]
+                });
+
+                return;
+            }
+
+            await updateDoc(userNotesRef, {
+                notes: arrayUnion({
+                    noteId: uuidv4(),
+                    title: "Nadpis",
+                    isImporatant: false,
+                    content: "Sem zadajte text.",
+                    created: new Date()
+                })
+            });
+
+            console.log("Note added successfully!");
+        } catch (error) {
+            console.error("Error adding note:", error);
+        }
+    }
 
     async function deleteNote(noteId){
-        //Odstranit z databazy
+        try {
+            const userNotesRef = doc(db, "notes", loggedUser.uid);
 
-        notes = notes.filter(note => note.noteId !== noteId);
+            const userNotesDoc = await getDoc(userNotesRef);
+
+            if (!userNotesDoc.exists()) {
+                console.error("User's note document not found.");
+                return;
+            }
+
+            let notes = userNotesDoc.data().notes;
+
+            const noteIndex = notes.findIndex(note => note.noteId === noteId);
+
+            if (noteIndex === -1) {
+                console.error("Note not found in user's document.");
+                return;
+            }
+
+            notes.splice(noteIndex, 1);
+
+            await updateDoc(userNotesRef, {
+                notes: notes
+            });
+
+        } catch (error) {
+            console.error("Error deleting note:", error);
+        }
     }
+
     async function deleteAllNotes(){
-        //Odstranit z databazy, listener a each statement?
+        try {
+            const userNotesRef = doc(db, "notes", loggedUser.uid);
+            const userNotesDoc = await getDoc(userNotesRef);
+
+            if (!userNotesDoc.exists()) {
+                console.error("User's note document not found.");
+                return;
+            }
+
+            let notes = userNotesDoc.data().notes;
+
+            notes = [];
+
+            await updateDoc(userNotesRef, {
+                notes: notes
+            });
+
+            console.log("All notes for user deleted successfully!");
+        } catch (error) {
+            console.error("Error deleting notes:", error);
+        }
     }
 
     async function toggleStar(noteId){
-        let note = document.getElementById(noteId);
+        try {
+            const userNotesRef = doc(db, "notes", loggedUser.uid);
+            const userNotesDoc = await getDoc(userNotesRef);
 
-        if (note){
-            //Updatnut v databaze
-            //Styl bude v svelte if statement, treba listener?
+            if (!userNotesDoc.exists()) {
+                console.error("User's note document not found.");
+                return;
+            }
+
+            let notes = userNotesDoc.data().notes;
+            const noteIndex = notes.findIndex(note => note.noteId === noteId);
+
+            if (noteIndex === -1) {
+                console.error("Note not found in user's document.");
+                return;
+            }
+
+            const importance = notes[noteIndex].isImportant;
+            delete notes[noteIndex].isImportant;
+            notes[noteIndex].isImportant = !importance;
+
+            // Update only the isImportant field of the specific note within the array
+            await updateDoc(userNotesRef, {
+                'notes': notes
+            });
+
+            console.log("Note star toggled successfully!");
+        } catch (error) {
+            console.error("Error toggling note star:", error);
         }
     }
 
@@ -67,6 +172,14 @@
     </div>
 
     <div class="notes">
+        {#if notes.length === 0}
+            <span
+                    class="no-notes-label"
+                    in:fade={{ delay: 200 , duration: 250 }}
+                    out:fade={{ duration: 100 }}
+            >Nemáte žiadne poznámky</span>
+        {/if}
+
         {#each notes as note}
             <div
                     class="note"
@@ -75,7 +188,7 @@
                     out:fade={{ duration: 100 }}
             >
                 <div class="star-mark">
-                    <md-icon-button on:click={toggleStar}>
+                    <md-icon-button on:click={() => toggleStar(note.noteId)}>
                         {#if note.isImportant}
                             <i class='bx bxs-star'></i>
                         {:else}
@@ -102,18 +215,22 @@
     </div>
 
 
-    <!-- Buttons //TODO nastylovat -->
-    <md-filled-tonal-button
-        on:click={addNote}
+    <!-- Buttons -->
+    <button
+            class="add-btn"
+            on:click={addNote}
     >
+        <i class='bx bx-plus'></i>
         Pridať poznámku
-    </md-filled-tonal-button>
+    </button>
 
-    <md-filled-button
-        on:click={deleteAllNotes}
+    <button
+            class="delete-all-btn"
+            on:click={deleteAllNotes}
     >
+        <i class='bx bxs-trash-alt'></i>
         Odstrániť všetky poznámky
-    </md-filled-button>
+    </button>
 </div>
 
 
@@ -125,6 +242,46 @@
         padding: 15px 9%;
         padding-bottom: 50px;
         box-shadow: var(--box-shadow) ; /* 0 5px 10px rgba(0, 0, 0, .1) */
+    }
+
+    .notes-container .add-btn{
+        display: inline-block;
+        background: var(--primary-button);
+        color: #fff;
+        font-size: 14px;
+        border-radius: 8px;
+        padding: 8px 15px;
+        border: none;
+    }
+
+    .notes-container .add-btn i{
+        transform: translateY(1px);
+    }
+
+    .notes-container .add-btn:hover{
+        background: var(--primary-button--hover);
+        transform: scale(1.01);
+        cursor: pointer;
+    }
+
+    .notes-container .delete-all-btn{
+        display: inline-block;
+        background: indianred;
+        color: #fff;
+        font-size: 14px;
+        border-radius: 8px;
+        padding: 8px 15px;
+        border: none;
+    }
+
+    .notes-container .delete-all-btn i{
+        transform: translateY(1px);
+    }
+
+    .notes-container .delete-all-btn:hover{
+        background: #f35353;
+        transform: scale(1.01);
+        cursor: pointer;
     }
 
     .notes-container .heading {
@@ -145,6 +302,10 @@
         display: grid;
         grid-template-columns: repeat(auto-fill, minmax(290px, 1fr));
         gap: 15px;
+    }
+
+    .notes-container .notes .no-notes-label{
+        color: var(--color-info);
     }
 
     .notes-container .notes .note{
